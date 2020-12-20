1204dc70129a04dfded36ea06afd0acaf9ec9758
Jose-HC <76268891+Jose-HC@users.noreply.github.com>
2020-12-19 19:52:59 -0600

    kk
    Co-Authored-By: Kovin <43118963+Slayer015@users.noreply.github.com>

--- mikrotik.py
+++ mikrotik.py
@@ -1,14 +1,13 @@
 import routeros_api
 import os
 import datetime, pathlib
+"""
 dt = datetime.datetime.now()
 fecha = str(dt.year)+'-'+str(dt.month)+'-'+str(dt.day)
 destino = os.path.join("C:/Users/jose_/Documents/TRABAJO ARANET/BACKUPS",fecha)
 os.mkdir(destino)
 
-"""
 array = ['','','']
-
 for i in array:
 """
 connection = routeros_api.RouterOsApiPool('192.168.10.16', username='admin', password='', plaintext_login=True)
